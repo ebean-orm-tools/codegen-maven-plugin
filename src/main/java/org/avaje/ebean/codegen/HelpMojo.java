30c9fa42f3bba2c58953ab69d16a5bd2bc37f756
Robin Bygrave <robin.bygrave@gmail.com>
2016-04-20 21:03:41 +1200

    update help output

--- src/main/java/org/avaje/ebean/codegen/HelpMojo.java
+++ src/main/java/org/avaje/ebean/codegen/HelpMojo.java
@@ -11,18 +11,21 @@ public class HelpMojo extends AbstractMojo {
 
     println("Help:");
     println(" ");
-    println("mvn ebean:init");
-    println("    ... use this to add properties, logging and MainDbMigration.java   ");
+    println("mvn ebean-codegen:help");
+    println("    ... output this message");
     println(" ");
-    println("mvn -Dpackage=<your package> ebean:generate-finders");
-    println("    ... use this to generate finders and attach them to entity beans   ");
-    println("    ... where <your package> is the package containing entity beans   ");
+    println("mvn ebean-codegen:init");
+    println("    ... use this to add properties, logging and MainDbMigration.java");
+    println(" ");
+    println("mvn -Dpackage=<your package> ebean-codegen:generate-finders");
+    println("    ... use this to generate finders and attach them to entity beans");
+    println("    ... where <your package> is the package containing entity beans");
     println("    ... e.g mvn -Dpackage=org.example.domain ebean:generate-finders");
     println(" ");
-    println("mvn -Dpackage=org.example.domain ebean:generate-finders-only");
+    println("mvn -Dpackage=org.example.domain ebean-codegen:generate-finders-only");
     println("    ... use this to generate finders only   ");
     println(" ");
-    println("mvn -Dpackage=org.example.domain ebean:generate-querybeans");
+    println("mvn -Dpackage=org.example.domain ebean-codegen:generate-querybeans");
     println("    ... use this to generate query beans (alternative to using the java annotation processor)");
     println(" ");
   }
